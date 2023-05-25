700d16968ab96afde68638c1c4909e133936971f
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-05-25 18:18:30 +0530

    solution update

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,14 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
 
+
+import React from 'react'
+
+const App = ()=>{
+
+    console.log("afda");
+    return(
+        <div>
+            <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+        </div>
+    )
+}
+export default App;
